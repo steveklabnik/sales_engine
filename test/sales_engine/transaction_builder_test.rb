4c5472db7055807924f0dbb4ffc853195928d8bf
Jennifer Eliuk <jen7maples@gmail.com>
2013-02-14 13:36:55 -0700

    add transaction tests

---
+++ test/sales_engine/transaction_builder_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require './lib/sales_engine/transaction_builder'
+
+class SalesEngine::TransactionBuilderTest < MiniTest::Unit::TestCase
+  def test_it_is_initialized_from_a_csv
+    data = "test/support/transactions_test.csv"
+    transactions = SalesEngine::TransactionBuilder.from_csv(data)
+    assert_equal(CSV, transactions.class)
+  end
+end
--- test/sales_engine/transaction_test.rb
+++ test/sales_engine/transaction_test.rb
@@ -4,14 +4,34 @@ require 'minitest/pride'
 require './lib/sales_engine/transaction'
 
 class SalesEngine::TransactionTest < MiniTest::Unit::TestCase
-  def test_it_exists
-    transaction = SalesEngine::Transaction.new
-    assert_kind_of(SalesEngine::Transaction, transaction)
+
+  def setup
+    @csv = CSV.open("test/support/transactions_test.csv", :headers => true, :header_converters => :symbol)
   end
 
-  def test_it_is_initialized_from_a_csv_of_data
-    data = "data/transactions.csv"
-    transactions = SalesEngine::Transaction.new(data).transactions
-    assert_equal(5595, transactions.count)
+  def test_it_exists
+    @first_record = @csv.first 
+    transaction = SalesEngine::Transaction.new(@first_record)
+    assert_kind_of(SalesEngine::Transaction, transaction)
+    assert_equal("4654405418249632", transaction.credit_card_number)
   end
+
+  def test_it_creates_array_of_transaction_objects
+    count = 1
+    @csv.each do |row|
+      SalesEngine::Transaction.create(row)
+    end
+    assert_operator(4, :<=, SalesEngine::Transaction.all.size)
+  end
+
+  def test_random_returns_random_transaction
+    @csv = CSV.open("test/support/transactions_test.csv", :headers => true, :header_converters => :symbol)
+    @csv.each do |row|
+      SalesEngine::Transaction.create(row)
+    end
+    rand1 = SalesEngine::Transaction.random 
+    rand2 = SalesEngine::Transaction.random 
+    refute_same(rand1.credit_card_number, rand2.credit_card_number)
+  end
+
 end
